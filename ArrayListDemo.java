a7c5a100ee981c5e8d70f89485e230789d9fa8c2
Jon Cooper <joncoop@users.noreply.github.com>
2018-01-29 09:08:20 -0500

    Add files via upload

---
+++ ArrayListDemo.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo
+{
+    public static void main(String[] twelve)
+    {
+        ArrayList<String> fruit = new ArrayList<String>();
+        
+        // How many things are in it?
+        System.out.println(fruit.size());
+        System.out.println(fruit.isEmpty());
+                
+        // We can add objects to the array one at a time
+        fruit.add("apple");
+        fruit.add("cucumber");
+        fruit.add("apple");
+        fruit.add("grape");
+        fruit.add("banana");
+        fruit.add("apple");
+        fruit.add("pear");
+        fruit.add("cucumber");
+        fruit.add("pear");
+        
+        // How many things are in it now?
+        System.out.println(fruit.size());
+        System.out.println(fruit.isEmpty());
+
+        // ArrayLists have useful toString methods
+        System.out.println(fruit);
+
+        // We can access individual elements.
+        System.out.println(fruit.get(3));
+
+        // Items can be found
+        System.out.println(fruit.indexOf("cucumber"));
+        
+        // More items can be added
+        fruit.add("kiwi");
+        fruit.add(4, "watermelon");
+        // Items can be removed
+        System.out.println(fruit.remove("pear"));   
+        System.out.println(fruit.remove(2));
+        
+        // Or we can overwrite elements
+        fruit.set(0, "jalapeno");
+            
+        // Just checking
+        System.out.println(fruit);
+        
+        // we can also traverse ArrayLists with loops
+        for (int i=0; i<fruit.size(); i++)
+            System.out.println(fruit.get(i));
+            
+        for (String f : fruit)
+            System.out.println(f);
+            
+        for (String f : fruit)
+            f = "guava";
+
+        System.out.println(fruit);
+        
+        
+        for (int i=0; i<fruit.size(); i++)
+            fruit.set(i, "guava");
+            
+        System.out.println(fruit);
+    }
+}
---
+++ ArrayListPractice.java
@@ -0,0 +1,64 @@
+/**
+ * AP Computer Science									Name ___________________________
+ * ArrayList Review/Self-Quiz
+ * 
+ * Read this code and ten
+ *
+ */
+ 
+import java.util.ArrayList;
+
+public class ArrayListPractice
+{
+	public static void main(String[] pirateSayings)
+	{
+		ArrayList<String> letters = new ArrayList<String>();
+		
+		// #0
+		System.out.println(letters.size() + " " + letters.isEmpty());
+								
+		// #1
+		letters.add("a");
+		letters.add("b");
+		letters.add("c");
+		letters.add("d");
+
+		System.out.println(letters.size() + " " + letters.isEmpty());
+
+		// #2 
+		System.out.println(letters);
+		System.out.println(letters.get(3));
+
+		// #3
+		letters.add("e");
+		letters.add(2,"x");
+		System.out.println(letters);
+				
+		// #4
+		letters.set(0,"z");
+		letters.remove("c");	
+		letters.remove(3);
+		System.out.println(letters);
+		
+		// #5
+		for (int i=letters.size()-1; i>=0; i--)
+			System.out.print(letters.get(i));
+		System.out.println();	
+		
+		// #6
+		for (String let: letters)
+			System.out.print(let);
+		System.out.println();
+			
+		// #7
+		ArrayList<String> more = new ArrayList<String>();
+		more.add("q");
+		more.add("r");
+		more.add("s");
+		
+		for (String let : more)
+			letters.add(0, let);
+			
+		System.out.println(letters);
+	}
+}
